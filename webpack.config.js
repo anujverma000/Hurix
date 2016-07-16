65ff7b3fb88a49e3789cc87014d841ee0118732a
Verma, Anuj (DS) <Anuj.Verma@cdk.com>
2016-07-17 00:04:07 +0530

    Project setup

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/assets/",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+};
